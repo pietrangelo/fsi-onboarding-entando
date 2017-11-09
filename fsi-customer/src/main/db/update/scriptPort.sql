bffa279601b53801bad10fafab3526e02e4cb13d
Matteo Emanuele Minnai <m.minnai@entando.com>
2017-11-09 15:19:26 +0100

    added widget for the registration form

---
+++ fsi-customer/src/main/db/update/scriptPort.sql
@@ -0,0 +1,10 @@
+--- 09-11-2017 Matteo
+
+INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('fsiApplicant_form', '<?xml version="1.0" encoding="UTF-8"?>
+<properties>
+<property key="en">Applicant Form</property>
+<property key="it">Form Applicant</property>
+</properties>', NULL, NULL, 'formAction', '<?xml version="1.0" encoding="UTF-8"?>
+<properties>
+<property key="actionPath">/ExtStr2/do/FrontEnd/jpapplicant/Applicant/new.action</property>
+</properties>', 1, 'free');
